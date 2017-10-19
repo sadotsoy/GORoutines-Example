6f9744c1075071616c302f2736ef46bffc4be7dd
Sadot Cortes <sadotcortes@scoorm.me>
2017-10-19 11:30:33 -0500

    Undirectional Channels implented

--- goRoutines+Channels/channel5.go
+++ goRoutines+Channels/channel5.go
@@ -5,21 +5,21 @@ import (
 	"time"
 )
 
-func genNumbers(out chan int) {
+func genNumbers(out chan<- int) {
 	for x := 1; x <= 5; x++ {
 		out <- x
 	}
 	close(out)
 }
 
-func squareNumbers(in chan int, out chan int) {
+func squareNumbers(in <-chan int, out chan<- int) {
 	for x := range in {
 		out <- x * x
 	}
 	close(out)
 }
 
-func printSquaredNumbers(in chan int) {
+func printSquaredNumbers(in <-chan int) {
 	for x := range in {
 		fmt.Println(x)
 		time.Sleep(1 * time.Second)
